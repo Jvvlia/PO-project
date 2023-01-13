8b976b5a3c08b1047f648f5caa002b968314ed47
zuza19 <100830685+zuza19@users.noreply.github.com>
2023-01-13 10:32:25 +0100

    Update SamochodWindow.xaml.cs

--- SamochodWindow.xaml.cs
+++ SamochodWindow.xaml.cs
@@ -65,8 +65,32 @@ namespace WypozyczalniaSamochodowGUI
                 if(CmbWyciagarka.Text == "tak")
                 { sam.Wyciagarka = EnumWyciagarka.tak; }
                 else { sam.Wyciagarka = EnumWyciagarka.nie; }
-
-                // czy z marka tez tak wypisywac ??
+                
+                if(CmbMarka.Text == "Audi")
+                { sam.Marka = EnumMarka.Audi; }
+                else { if(CmbMarka.Text == "BMW") { sam.Marka = EnumMarka.BMW; }
+                else { if(CmbMarka.Text == "Citroen") { sam.Marka = EnumMarka.Citroen; }
+                else { if(CmbMarka.Text == "Dacia") { sam.Marka = EnumMarka.Dacia; } 
+                else { if(CmbMarka.Text == "Fiat") { sam.Marka = EnumMarka.Fiat; }
+                else { if(CmbMarka.Text == "Ford") { sam.Marka = EnumMarka.Ford; }
+                else { if(CmbMarka.Text == "Hyundai") { sam.Marka = EnumMarka.Hyundai; }
+                else { if(CmbMarka.Text == "Kia") { sam.Marka = EnumMarka.Kia; }
+                else { if(CmbMarka.Text == "Mercedes") { sam.Marka = EnumMarka.Mercedes; }
+                else { if(CmbMarka.Text == "Nissa") { sam.Marka = EnumMarka.Nissan; }
+                else { if(CmbMarka.Text == "Opel") { sam.Marka = EnumMarka.Opel; }
+                else { if(CmbMarka.Text == "Peugeot") { sam.Marka = EnumMarka.Peugeot; }
+                else { if(CmbMarka.Text == "Renault") { sam.Marka = EnumMarka.Renault; }
+                else { if(CmbMarka.Text == "SEAT") { sam.Marka = EnumMarka.SEAT; }
+                else { if(CmbMarka.Text == "Skoda") { sam.Marka = EnumMarka.Skoda; }
+                else { if(CmbMarka.Text == "Toyota") { sam.Marka = EnumMarka.Toyota; }
+                else { if(CmbMarka.Text == "Volkswagen") { sam.Marka = EnumMarka.Volkswagen; }
+                else { if(CmbMarka.Text == "Volvo") { sam.Marka = EnumMarka.Volvo; }
+                
+                /*switch (CmbMarka)
+                {
+                    case CmbMarka.Text == "Audi": sam.Marka = EnumMarka.Audi; break;
+                    case CmbMarka.Text == "BMW":sam.Marka = EnumMarka.BMW; break;
+                }*/
 
                 sam.Model = TxtModel.Text;
                 sam.NumerRejestracyjny = TxtNrRejestracyjny.Text;
